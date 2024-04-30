2797deffb0d37369caba4112c4d47786b09021e9
JorgeLG87 <jorge85_6@hotmail.com>
2024-04-30 19:46:46 -0400

    Updated form.jsx and form.css

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -7,7 +7,32 @@ export default function Form() {
                 <img className="menu" src="src/assets/menu.png"/>
             </div>
             <form className="order-form">
-                <label>
+                <label className="firstname-container">
+                    First Name:
+                    <input className="name-input" type="text" required></input>
+                </label>
+                <label className="lastname-container">
+                    Last Name:
+                    <input className="lastname-input" type="text" required></input>
+                </label>
+                <label className="delivery-container">
+                    Delivery Address:
+                    <input className="delivery-input" required></input>
+                </label>
+                <label className="city-container">
+                    City:
+                    <input className="city-input" required></input>
+                </label>
+                <label className="state-container">
+                    State:
+                    <input className="state-input" required></input>
+                </label>
+                <label className="contact-container">
+                    Contact Number:
+                    <input className="contact-number" type="number" required></input>
+                </label>
+                {/* <div className="ordermenu-container"> */}
+                <label className="lomo-container">
                     Choose your type:
                     <select className="selection">
                         <option value="house-lomo">Lomo the G.O.A.T</option>
@@ -15,7 +40,7 @@ export default function Form() {
                     </select>
                 </label>
                 <br/><br/>
-                <label>
+                <label className="beverage-container">
                     Beverage:
                     <select className="selection">
                         <option value="pepsi">Pepsi</option>
@@ -23,6 +48,17 @@ export default function Form() {
                         <option value="orange juice">Orange Juice</option>
                     </select>
                 </label>
+                <label className="french-fries">
+                    French Fries:
+                    <select className="selection">
+                        <option value="none">------None------</option>
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                    </select>
+                </label>
+                {/* </div> */}
+                <button className="submit-btn" type="submit">Place Order</button>
             </form>
         </div>
     )
--- src/Components/QueEsLomo.jsx
+++ src/Components/QueEsLomo.jsx
@@ -5,14 +5,14 @@ export default function QueEsLomo() {
         <div className="queeslomo-container">
             <img className="lomo-imagen" src="src/assets/lomito-argentino.jpeg"/>
             <div className="explanation">
-            <p className="lomo-title">What is a lomito?</p>
+            <p className="lomo-title">"Lomito"? What is that!?</p>
             <p className="lomo-text"><span className="tricks">-----</span>A lomito is a high quality argentinean steak sandwich. There are different styles of lomitos within Argentina, but we speacialize in a unique one that is made in <span className="san-juan">San Juan</span>.</p>
-            <p className="lomo-text"><span className="tricks">-----</span>From our home made bread to our high quality steak, we  </p>
+            <p className="lomo-text"><span className="tricks">-----</span>From our home made bread to our high quality steak, we focus not only on a top notch sandwich but also in providing a great experience to our customers. So without further a due, let's welcome the authentic <span className="san-juan">Argentinean lomito </span>to <span className="san-juan">New York City and New Jersey!</span></p>
             </div>
             <div className="mapa-arg-container">
             <img className="mapa-arg" src="src/assets/sanjuan.png"/>
             <img className="bandera" src="src/assets/bandera-argentina2.png"/>
-            <p>Up to Date</p>
+            {/* <p>Up to Date</p> */}
             </div>
         </div>
     )
