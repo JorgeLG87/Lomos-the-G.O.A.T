bb8bf65773179ccc715f360d3a6737aa6715772a
JorgeLG87 <jorge85_6@hotmail.com>
2024-06-07 09:30:14 -0400

    Added TikTok link to SocialMedia.jsx

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -7,7 +7,7 @@ export default function SocialMedia() {
             <div className="socialmedia-container">
             <a href="https://www.instagram.com/lomostheg.o.a.t/"><img className="instagram" src="https://img.icons8.com/color/144/instagram-new--v1.png" alt="instagram-new--v1" /></a>
             <a href="https://www.facebook.com/people/Lomos-The-G-o-a-t/61559046853292/"><img className="facebook" src="https://img.icons8.com/color/144/facebook-new.png" alt="facebook-new"/></a>
-            <img className="tiktok" src="https://img.icons8.com/color/144/tiktok--v1.png" alt="tiktok--v1"/>
+            <a href="https://www.tiktok.com/@lomostheg.o.a.t?lang=en"><img className="tiktok" src="https://img.icons8.com/color/144/tiktok--v1.png" alt="tiktok--v1"/></a>
             </div>
         </div>
     )
