33a9a0a49137dc632e5222c94272f19715487cf9
JorgeLG87 <jorge85_6@hotmail.com>
2024-06-28 07:16:49 -0400

    Fixed some CSS issues

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -6,9 +6,15 @@ export default function AboutUs() {
         <div className="title-span-container-aboutus">
             <p className="title2"><span className="span-text"># Meet Our</span></p><p className="title">Team</p>
             <div className="photos-container">
-                <div className="photo1"></div>
-                <div className="photo2"></div>
-                <div className="photo3"></div>
+                <div className="photo1">
+                    <img className="first-photo" src="/cataratas-Iguazu.jpg"/>
+                </div>
+                <div className="photo2">
+                    <img className="second-photo" src="/cataratas-Iguazu.jpg"/>
+                </div>
+                <div className="photo3">
+                    <img className="third-photo" src="/cataratas-Iguazu.jpg"/>
+                </div>
             </div>
         </div>
         </>
