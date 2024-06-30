469aab0b790e3481dc8d3c73d32b1d22d9b07d53
JorgeLG87 <jorge85_6@hotmail.com>
2024-06-30 10:12:45 -0400

    Changed the width of the sliding window in home page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,15 +5,20 @@ export default function AboutUs() {
         <>
         <div className="title-span-container-aboutus">
             <p className="title2"><span className="span-text"># Meet Our</span></p><p className="title">Team</p>
-            <div className="photos-container">
-                <div className="photo1">
-                    <img className="first-photo" src="/cataratas-Iguazu.jpg"/>
+            <div className="container-body-aboutUs">
+                <div className="photos-container">
+                    <div className="photo1">
+                        <img className="first-photo" src="/cataratas-Iguazu.jpg"/>
+                    </div>
+                    <div className="photo2">
+                        <img className="second-photo" src="/cataratas-Iguazu.jpg"/>
+                    </div>
+                    <div className="photo3">
+                        <img className="third-photo" src="/cataratas-Iguazu.jpg"/>
+                    </div>
                 </div>
-                <div className="photo2">
-                    <img className="second-photo" src="/cataratas-Iguazu.jpg"/>
-                </div>
-                <div className="photo3">
-                    <img className="third-photo" src="/cataratas-Iguazu.jpg"/>
+                <div className="text-aboutUs-container">
+                    <p>With one dream in mind, a group of friends from San Juan (Argentina) decided to team up and work together towards one goal in mind. After experiencing the variety of cuisines within USA, their mission was to bring a unique flavor to North America. By the hand</p>
                 </div>
             </div>
         </div>
