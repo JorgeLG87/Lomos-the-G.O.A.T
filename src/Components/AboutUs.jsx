ead00fdb49e0d9f46a333ebeb14e50efcb2249e4
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-02 12:43:06 -0400

    Fixed styling for the About Us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -18,8 +18,13 @@ export default function AboutUs({ slides, AboutUsCarousel }) {
                     </div>
                 </div> */}
                 <AboutUsCarousel slides={slides}/>
+                <div>
+                <div className="idea-container">
+                    <img className="lightbulb" src="/lightbulb.svg" alt="Lightbulb icon"/>
+                </div>
                 <div className="text-aboutUs-container">
                     <p style={{fontFamily:"Montserrat"}}>With one dream in mind, a group of friends from San Juan (Argentina) decided to team up and work together towards one goal in mind. After experiencing the variety of cuisines within USA, their mission was to bring a unique flavor to North America. By the hand</p>
+                </div>
                 </div>
             </div>
         </div>
