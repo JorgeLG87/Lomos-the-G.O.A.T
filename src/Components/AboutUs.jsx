41caf52db73ddd3522b6dc33efbb88b4106e947f
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-13 19:51:23 -0400

    Fixed CSS issues

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -6,17 +6,6 @@ export default function AboutUs({ slides, AboutUsCarousel }) {
         <div className="title-span-container-aboutus">
             <p className="title2"><span className="span-text"># Meet Our</span></p><p className="title">Team</p>
             <div className="container-body-aboutUs">
-                {/* <div className="photos-container">
-                    <div className="photo1">
-                        <img className="first-photo" src="/cataratas-Iguazu.jpg"/>
-                    </div>
-                    <div className="photo2">
-                        <img className="second-photo" src="/cataratas-Iguazu.jpg"/>
-                    </div>
-                    <div className="photo3">
-                        <img className="third-photo" src="/cataratas-Iguazu.jpg"/>
-                    </div>
-                </div> */}
                 <AboutUsCarousel slides={slides}/>
                 <div className="idealogo-text-container">
                     <div className="idea-container">
