bc66e32a95fbb9e1ab98f264018b6e55a6136c23
JorgeLG87 <jorge85_6@hotmail.com>
2024-10-24 08:26:00 -0400

    CSS Styling

--- src/Components/QueEsLomo.jsx
+++ src/Components/QueEsLomo.jsx
@@ -44,7 +44,7 @@ export default function QueEsLomo() {
             <div className="explanation">
                 <p className="container-lomito-text"><span className="span-lomito-text">LOMITO?</span></p><p className="lomo-title">What is that!?</p>
                 <p className="lomo-text"><span className="tricks">-----</span>A lomito is a high quality argentinean steak sandwich. There are different styles of lomitos within Argentina, but we specialize in a unique one that is made in <span className="san-juan">San Juan</span>.</p>
-                <p className="lomo-text"><span className="tricks">-----</span>From our home made bread to our high quality steak, we focus not only on a top notch sandwich but also in providing a great experience to our customers. So without further a due, let's welcome the authentic <span className="san-juan">Argentinean lomito </span>to <span className="san-juan">New York City and New Jersey!</span></p>
+                <p className="lomo-text"><span className="tricks">-----</span>From our home made bread to our high quality steak, we focus not only on a top notch sandwich but also in providing a great experience to our customers. So without further a due, let's welcome the authentic <span className="san-juan">Argentinean lomito </span>to the<span className="san-juan"> USA!</span></p>
             </div>
             <div className="mapa-arg-container" ref={mapSanJuan}>
                 <img className="mapa-arg" src="/sanjuan.png"/>
