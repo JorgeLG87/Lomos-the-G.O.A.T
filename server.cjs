bf27e25201b8d32bc553c54ed9e1cd36a277277b
JorgeLG87 <jorge85_6@hotmail.com>
2024-12-09 16:47:25 -0500

    Server issue

--- server.cjs
+++ server.cjs
@@ -15,6 +15,12 @@ app.use(express.static(path.join(__dirname, "build")));
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+
 app.post("/checkout", async (req, res) => {
 
     console.log(req.body);
