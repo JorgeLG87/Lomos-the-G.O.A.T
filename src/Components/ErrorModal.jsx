31648ff3b998058ad13341bece088d99f2fec0c3
JorgeLG87 <jorge85_6@hotmail.com>
2024-12-18 07:21:52 -0500

    Added a dynamic message to display in error box message

--- src/Components/ErrorModal.jsx
+++ src/Components/ErrorModal.jsx
@@ -1,14 +1,28 @@
 import "./ErrorModal.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { CartContext } from "../cartContext";
 
 export default function ErrorModal({ setIsOpen }) {
     // const [ isOpen, setIsOpen ] = useState(false);
+    
+    const cart = useContext(CartContext);
 
-    return (
-        <div className="modal-main-container-open">
-            <p className="modal-text-title">Oopss..</p>
-            <p className="modal-text">Please enter customer's info to proceed with checkout.</p>
-            <button className="modal-btn" type="button" onClick={() => setIsOpen(false)}>Ok</button>
-        </div>
-    )
+    if (cart.items.length === 0) {
+        return (
+            <div className="modal-main-container-open">
+                <p className="modal-text-title">Oopss..</p>
+                <p className="modal-text">Your cart is empty. Please add items in order to proceed with checkout.</p>
+                <button className="modal-btn" type="button" onClick={() => setIsOpen(false)}>Ok</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className="modal-main-container-open">
+                <p className="modal-text-title">Oopss..</p>
+                <p className="modal-text">Please add customer's information in order to proceed with checkout.</p>
+                <button className="modal-btn" type="button" onClick={() => setIsOpen(false)}>Ok</button>
+            </div>
+        )
+    }
+    
 }
