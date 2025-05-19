7cee90b6c1cb821b60f4583cd1a9bbaf3b302e20
JorgeLG87 <jorge85_6@hotmail.com>
2025-05-18 20:56:38 -0400

    Add proper header to avoid CORS issues

--- server.cjs
+++ server.cjs
@@ -19,11 +19,11 @@ app.use(express.static(path.join(__dirname, "build")));
 
 app.use(express.json());
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//  })
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 
 app.get("/verify-payment", async (req, res) => {
     const sessionId  = req.query.session_id;
