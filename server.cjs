dd879a02ac5ca6c8eeac37080b718a0b3355dd5d
JorgeLG87 <jorge85_6@hotmail.com>
2025-05-18 21:08:23 -0400

    Try to fix CORS issues

--- server.cjs
+++ server.cjs
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin");
+    res.header("Access-Control-Allow-Origin", "https://lomosthegoat.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  })
